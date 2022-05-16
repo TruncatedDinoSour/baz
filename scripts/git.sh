56766f0dbfb74cb6bfc81573b23279b0e244a34b
Ari Archer <ari.web.xyz@gmail.com>
2022-05-16 22:10:58 +0300

    update @ Mon 16 May 22:10:58 EEST 2022
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/git.sh
+++ scripts/git.sh
@@ -3,6 +3,13 @@
 set -e
 
 main() {
+    echo 'Did you change the version??'
+    printf 'Press enter to mean yes'
+    read -r
+
+    echo 'Okay.. I will sleep for couple of seconds just in case'
+    sleep 4s
+
     git add -A
     git commit -sam "${m:-"update @ $(date)"}"
     git push -u origin "$(git rev-parse --abbrev-ref HEAD)"
