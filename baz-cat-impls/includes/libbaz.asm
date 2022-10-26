bfccdbb4b5dc31ddb2bca79f68b6de74cbd71086
Ari Archer <ari.web.xyz@gmail.com>
2022-10-26 15:36:32 +0300

    4.1.0: Improve baz-cat, and baz loader performance
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- baz-cat-impls/includes/libbaz.asm
+++ baz-cat-impls/includes/libbaz.asm
@@ -9,7 +9,7 @@ define STDOUT_FILENO 1
 
 define EXIT_OK       0
 
-define BUF_SIZE      64  ;; You really don't need much, this allocates 64 bytes
-                         ;; in the memory, baz-cat usually gets called with smaller
-                         ;; rather than bigger input, if expected the other way around
-                         ;; I'd suggest using like 256 KB, or in this case -- 262144 bytes
+define BUF_SIZE      128  ;; You really don't need much, this allocates 128 bytes
+                          ;; in the memory, baz-cat usually gets called with smaller
+                          ;; rather than bigger input, if expected the other way around
+                          ;; I'd suggest using like 256 KB, or in this case -- 262144 bytes
